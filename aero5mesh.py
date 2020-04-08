1074288f206ae2c31a17dcb1b1f9688dcdf1acec
Victor Santos <victorsantos@ufmg.br>
2020-04-08 13:33:24 -0300

    change to work with any path

--- aero5mesh.py
+++ aero5mesh.py
@@ -3,10 +3,11 @@ from aero.AeroelasticPanels import SuperAeroPanel5
 from aero.AeroelasticPostProcessing import plot_flutter_data, plot_critical_flutter_data, export_flutter_data, read_f06
 from pyNastran.utils.nastran_utils import run_nastran
 from femap.femap import Femap
+import pathlib
 import os
 
 if __name__ == '__main__':
-    base_path = r'C:\Users\Victor\PycharmProjects\aero-5-mesh-gen'
+    base_path = pathlib.Path().absolute()
     input_file = os.path.join(base_path, 'input-model.bdf')
     output_file = os.path.join(base_path, 'output-model.bdf')
     nastran_exe = r'D:\Programs\MSC.Software\NaPa_SE\2019fp1\Nastran\bin\nast20191.exe'
@@ -28,4 +29,4 @@ if __name__ == '__main__':
     modes, critical_modes, flutter = read_f06(output_file.replace('.bdf', '.f06'), analysis.subcases[1])
     plot_flutter_data(modes, analysis.subcases[1])
     plot_critical_flutter_data(critical_modes, analysis.subcases[1])
-    export_flutter_data(modes, critical_modes, flutter, analysis.subcases[1], os.path.join(base_path, 'test.xlsx'))
+    export_flutter_data(modes, critical_modes, flutter, analysis.subcases[1], os.path.join(base_path, 'output-model.xlsx'))
